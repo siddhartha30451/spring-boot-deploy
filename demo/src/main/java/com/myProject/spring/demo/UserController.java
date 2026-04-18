ba225a3dd97fe89f7ebfccd2a95b90b1cf512ab2
siddhartha30451 <savaramsiddhartha05@gmail.com>
2026-04-18 18:07:44 +0530

    initial commit

---
+++ demo/src/main/java/com/myProject/spring/demo/UserController.java
@@ -0,0 +1,14 @@
+package com.myProject.spring.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Hello User! welcome to my App";
+    }
+}
