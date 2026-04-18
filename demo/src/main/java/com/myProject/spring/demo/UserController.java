7eaa65ea659c24a23325db548c4746fa52c8b7fd
siddhartha30451 <savaramsiddhartha05@gmail.com>
2026-04-18 19:15:25 +0530

    feature change

--- demo/src/main/java/com/myProject/spring/demo/UserController.java
+++ demo/src/main/java/com/myProject/spring/demo/UserController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("/welcome")
     public String welcome(){
-        return "Hello User! welcome to my App";
+        return "Hello User! welcome to my App. you are now in feature-test branch";
     }
 }
